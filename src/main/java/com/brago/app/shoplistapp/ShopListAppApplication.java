67325ab747f84652f9244858d3906c83366ff47b
Brayan Esguerra <brago0822@gmail.com>
2022-12-08 23:20:59 -0500

    [1]: First version project files

---
+++ src/main/java/com/brago/app/shoplistapp/ShopListAppApplication.java
@@ -0,0 +1,13 @@
+package com.brago.app.shoplistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopListAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brago/app/shoplistapp/ShopListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brago.app.shoplistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
